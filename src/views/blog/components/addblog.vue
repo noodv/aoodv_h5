a741f3683cee6d6e47173505c4bbc00ccc6bca85
diesel <noodv@sina.com>
2019-12-13 01:45:24 +0800

    fdfdsa

--- src/views/blog/components/addblog.vue
+++ src/views/blog/components/addblog.vue
@@ -1,23 +1,18 @@
 <template>
   <div>
-    <mt-header title="编辑博客">
+    <mt-header title="编辑话题">
       <router-link to="" @click.native="back" slot="left">
         <mt-button class="back-button">返回</mt-button>
       </router-link>
       <!-- <mt-button icon="more" slot="right" @click="bottomPopupVisible=true"></mt-button> -->
     </mt-header>
-    <mt-field label="标题" placeholder="（必填）" v-model="formData.username"></mt-field>
+    <mt-field label="标题" placeholder="（必填）" v-model="formData.subject"></mt-field>
     <mt-field label="分类" placeholder="请选择" type="email" v-model="formData.typeName" readonly @click.native="bottomPopupVisible=true"></mt-field>
     <div class="title">
       <span class="title">内容</span>
     </div>
-    <mt-field
-      placeholder="文章"
-      type="textarea"
-      rows="4"
-      v-model="formData.typeId"
-    ></mt-field>
-
+    <div ref="content" placeholder='请输入' class="content edit" contentEditable='true' v-html="formData.message">
+    </div>
     <div class="ebox imagebox" style="display:block;">
       <ul id="imglist2" class="bg-f  p5">
         <li v-if="true" class="webuploader-container">
@@ -25,7 +20,7 @@
             <a><i class="iconfont icon-tupian bigs"></i></a>
           </div>
           <div id="rt_rt_1drl85l4lp181ejl14rd1fcvpc91" style="position: absolute; top: 0px; left: 0px; width: 50px; height: 44px; overflow: hidden; bottom: auto; right: auto;">
-            <input ref="upload" type="file" name="file" class="webuploader-element-invisible" multiple="multiple" accept="image/*,text/plain,application/msword,application/octet-stream,application/vnd.ms-excel,application/x-shockwave-flash">
+            <input ref="upload" @change="addImg" type="file" name="file" class="webuploader-element-invisible" multiple="multiple" accept="image/*,text/plain,application/msword,application/octet-stream,application/vnd.ms-excel,application/x-shockwave-flash">
             <label style="opacity: 0; width: 100%; height: 100%; display: block; cursor: pointer; background: rgb(255, 255, 255);" @click="onUpload"></label>
           </div>
         </li>
@@ -37,7 +32,7 @@
       </ul>
     </div>
 
-    <mt-button style="width: 100%; margin: 10px auto;" type="primary">发布</mt-button>
+    <mt-button style="width: 100%; margin: 10px auto;" type="primary" @click="saveBlogPost">发布</mt-button>
     <mt-popup class="sharePopup" v-model="bottomPopupVisible" position="bottom" popup-transition="popup-fade" >
       <mt-picker style="height: 100%;" :slots="slots" valueKey="name" @change="changeType"></mt-picker>
     </mt-popup>
@@ -46,7 +41,7 @@
 
 <script>
 import { Indicator } from 'mint-ui'
-import { saveBlogPost, findTechTypeList } from '@/api'
+import { saveBlogPost, findTechTypeList, getBaseUrl } from '@/api'
 
 export default {
     data() {
@@ -56,7 +51,7 @@ export default {
         formData: {
           subject: '',
           message: '',
-          fid: '',
+          typeId: '',
           typeName: ''
         },
         slots: [
@@ -81,10 +76,44 @@ export default {
           })
         })
       },
+      addImg(event) {
+        let inputDOM = this.$refs.upload;
+        // 通过DOM取文件数据
+        this.fil = inputDOM.files
+        let len = this.fil.length
+        if(len>4){
+          alert('最多可上传4张，您还可以上传'+(4-oldLen)+'张');
+          return false;
+        }
+        for (let i=0; i < this.fil.length; i++) {
+          let size = Math.floor(this.fil[i].size / 1024);
+          if (size > 5*1024*1024) {
+            alert('请选择5M以内的图片！')
+            return false
+          }
+          this.uploadImg('/blogpost/UploadFile' ,this.fil[i])
+        }
+      },
+      uploadImg(url, file) {
+        var _this = this
+        var fromData = new FormData()
+        fromData.append("file", file)
+        var oAjax = new XMLHttpRequest()
+        oAjax.open('post', `${getBaseUrl() + url}`, true)
+        oAjax.send(fromData);
+        oAjax.onreadystatechange = function() {
+          if (oAjax.readyState == 4) {
+            let { code, obj } = JSON.parse(oAjax.response)
+            if (code == 200 && oAjax.status >= 200 && oAjax.status < 300 || oAjax.status == 304) {
+              _this.$refs.content.innerHTML = _this.$refs.content.innerHTML + `<p><img style='width: 100%;height: 200px;' src="${obj}" /></p>&nbsp;`
+            }
+          }
+        }
+      },
       changeType(picker, values) {
         if (values[0] != undefined) {
           this.formData.typeName = values[0].name
-          this.formData.fid = values[0].value
+          this.formData.typeId = values[0].value
           this.bottomPopupVisible = false
         }
       },
@@ -99,8 +128,10 @@ export default {
           return
         }
         this.isAjax = false
-        saveTopic(this.formData).then(res => {
+        this.formData.message = this.$refs.content.innerHTML
+        saveBlogPost(this.formData).then(res => {
           this.isAjax = false
+          this.$router.push({path: '/topicview'})
         }).catch(() => {
           this.isAjax = false
         })
@@ -117,19 +148,25 @@ body, ul, ol, li, dl, dd, p, h1, h2, h3, h4, h5, h6, form, fieldset, .pr, .pc {
     margin: 0;
     padding: 0;
 }
-.title {
-  height: 1.2rem;
-  background-color: #f7f9fc;
-  line-height: 1.2rem;
-}
 .back {
   font-size: 0.8rem;
   margin-left: 0.3rem;
 }
 .title {
+  height: 1.2rem;
+  background-color: #f7f9fc;
+  line-height: 1.2rem;
+}
+.title .title {
   font-size: 0.43rem;
   position: absolute;
   left: 3%;
+}
+.content {
+  width: 97%;
+  min-height: 200px;
+  padding: 5px 5px;
+  font-size: 15px;
 }
 .sharePopup {
   width: 100%
@@ -164,5 +201,10 @@ ul, .itemlist {
 }
 .webuploader-element-invisible {
     display: none;
+}
+.edit,  
+.edit * {  
+    -webkit-user-select: auto;  
+    -webkit-user-modify: read-write;  
 }
 </style>
